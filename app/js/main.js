74686fcf588839c4ed890fec8fe4975b504eb980
alienReyes <kid.alien@gmail.com>
2017-01-23 18:46:49 -0500

    back to bussiness

--- app/js/main.js
+++ app/js/main.js
@@ -6,41 +6,5 @@ $(document).ready(function() {
   });
   });
 
-  var speed =250,
-    easing = mina.easeinout;
-
 $('body').on('click', 'figure', function(e) {
 })
-
-
-
-function animateHeader () {
-  	var s = Snap( '#decoration');
-     path = s.select( 'path' );
-    pathConfig = {
-      from : path.attr( 'd' ),
-      to : "M1366,103L0,195V0h1366V103z"
-    };
-    	path.animate( { 'path' : pathConfig.to }, speed, easing );
-
-}
-
-	// function init() {
-  //
-  //
-	// 	[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
-	// 		var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
-	// 			pathConfig = {
-	// 				from : path.attr( 'd' ),
-	// 				to : "M1366,203L0,195V0h1366V303z"
-	// 			};
-  //
-	// 		el.addEventListener( 'mouseenter', function() {
-	// 			path.animate( { 'path' : pathConfig.to }, speed, easing );
-	// 		} );
-  //
-	// 		el.addEventListener( 'mouseleave', function() {
-	// 			path.animate( { 'path' : pathConfig.from }, speed, easing );
-	// 		} );
-	// 	} );
-	// }
