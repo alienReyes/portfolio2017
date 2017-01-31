775720e0afe3ee318fc639b6a346c72941d85fe3
alienReyes <kid.alien@gmail.com>
2017-01-30 19:14:54 -0500

    scale working
    issue with total height

--- app/js/main.js
+++ app/js/main.js
@@ -5,21 +5,39 @@ docElem = window.document.documentElement;
 
 
 $(document).ready(function() {
+
+  //json for template
   $.getJSON('js/data.json', function(data) {
      var template = document.getElementById('buttons__Grid').innerHTML;
   var html = Mustache.to_html(template, data);
   $('.grid').html(html);
   });
+
+
+
+
   });
 
-$('body').on('click', '.thumb--item', function(e) {
-//  $(this).toggleClass('selected');
 
-  //setTimeout(function() {
-		//		  loadContent($(this))
-			//	}, 300);
+  function loadGallery (item){
+    console.log(item);
+    var dummy = document.createElement ('div');
+      $(dummy).addClass('placeholder');
+      var offset = $(item).offset();
+  //  $(dummy).addClass( 'trans-in');
+// set the width/heigth and position
+      dummy.style.WebkitTransform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + $(item).width()/gridItemsContainer.width() + ',' + $(item).height()/getViewport('y') + ',1)';
 
-})
+
+    dummy.style.transform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + $(item).width()/gridItemsContainer.width() + ',' + $(item).height()/getViewport('y') + ',1)';
+
+    $('.grid-container').append(dummy);
+
+    TweenMax.to(dummy, .5, {ease: Expo.easeInOut, scaleX:1,scaleY:1,x:0,y:0})
+
+  }
+
+
 
 $('body').on({
     mouseenter: function (e) {
@@ -33,46 +51,10 @@ $('body').on({
 }, ".thumb--item");
 
 
-
-
-
-function init() {
-  initEvents();
-}
-
-// function initEvents (){
-// $('.thumb--item').click(function(e){
-//   $(this).preventDefault();
-//   if(isAnimating || current === pos) {
-//         return false;
-//       }
-// isAnimating = true;
-//
-//
-// })
-function loadContent (item){
-  var dummy = document.createElement ('div');
-//  $(dummy).addClass('placeholder');
-var offset = item.offset();
-$(dummy).addClass( 'placeholder');
-$(dummy).addClass( 'trans-in');
-
-
-
-  // set the width/heigth and position
-  dummy.style.WebkitTransform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + item.width()/gridItemsContainer.width() + ',' + item.height()/getViewport('y') + ',1)';
-console.log( item.offsetWidth)
-dummy.style.transform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + item.width()/gridItemsContainer.width() + ',' + item.height()/getViewport('y') + ',1)';
-
-
-;
-  $('.grid-container').append(dummy);
-  setTimeout(function() {
-
-      }, 200);
-
-
-}
+$( "body" ).on( "click", ".thumb--item", function(e) {
+  var block =$(this).find('.block--color')
+  TweenMax.to(block, .5, {ease: Expo.easeInOut, y:0, onComplete:loadGallery, onCompleteParams:$(this)})
+});
 
 
 function getViewport( axis ) {
@@ -88,3 +70,40 @@ function getViewport( axis ) {
 
   return client < inner ? inner : client;
 }
+
+
+
+/*
+
+
+ function initEvents (){
+ $('.thumb--item').click(function(e){
+   $(this).preventDefault();
+   if(isAnimating || current === pos) {
+         return false;
+       }
+ isAnimating = true;
+
+
+ })
+
+var dummy = document.createElement ('div');
+  $(dummy).addClass('placeholder');
+  var offset = item.offset();
+$(dummy).addClass( 'placeholder');
+$(dummy).addClass( 'trans-in');
+   set the width/heigth and position
+  dummy.style.WebkitTransform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + item.width()/gridItemsContainer.width() + ',' + item.height()/getViewport('y') + ',1)';
+console.log( item.offsetWidth)
+dummy.style.transform = 'translate3d(' + (offset.left - 5) + 'px, ' + (offset.top - 5) + 'px, 0px) scale3d(' + item.width()/gridItemsContainer.width() + ',' + item.height()/getViewport('y') + ',1)';
+
+
+;
+  $('.grid-container').append(dummy);
+  setTimeout(function() {
+
+      }, 200);
+
+
+}
+*/
